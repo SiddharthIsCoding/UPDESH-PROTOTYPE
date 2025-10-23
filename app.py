9ad3b894c335aeb2f44ea4883a14e22204ba8340
siddbytheway <88389185+SiddharthIsCoding@users.noreply.github.com>
2025-10-23 17:23:48 +0530

    project initiation

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import *
+
+
+
+import pyrebase
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return render_template("index.html")
+
+
+
+@app.route("/login")
+
+def login():
+    return render_template("login.html")
+
+
+
+
+
+
+app.run(debug=True)
